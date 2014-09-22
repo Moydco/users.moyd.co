649d8b46f014a15b403288f2ca1ad0b13d15222b
Alberto Zuin <alberto@azns.it>
2014-09-22 13:35:07 +0100

    Consumes now is authorized by application and not by user token

--- app/controllers/consumes_controller.rb
+++ app/controllers/consumes_controller.rb
@@ -1,27 +1,28 @@
 class ConsumesController < ApplicationController
-  before_action :signed_in_user
+  #before_action :signed_in_user
 
   # Debit user with "amount". Amount is in pence (UKP * 100)
   def create
     @amount = params[:amount]
-    @user = User.find(params[:user_id])
-    logger.info "User id #{@user.id.to_s}"
-    logger.info "User id from token #{current_user.id.to_s}"
-    logger.info "User balance #{current_user.balance.to_s}"
+    @user = User.where(id: params[:user_id]).first
 
-    if @user != current_user
-      render text: 'User not correct', status: 500
-    else
-      if @user.balance < params[:amount].to_i
-        render text: 'User has not enough money', status: 403
+    if params[:client_id] == application_id and params[:client_secret] == application_secret
+      if @user.nil?
+        render text: 'User not correct', status: 500
       else
-        activity = @user.activities.create(kind: 'consume', amount: @amount)
-        c = Consume.create(description: params[:description])
-        activity.consume = c
-        activity.save
+        if @user.balance < params[:amount].to_i
+          render text: 'User has not enough money', status: 403
+        else
+          activity = @user.activities.create(kind: 'consume', amount: @amount)
+          c = Consume.create(description: params[:description])
+          activity.consume = c
+          activity.save
 
-        render text: 'User billed', status: 200
+          render text: 'User billed', status: 200
+        end
       end
+    else
+      render text: 'Application authentication failed'
     end
   end
 end
